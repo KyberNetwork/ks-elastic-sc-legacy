60718f552ad46a473a050e8db6e499724227f222
ducquangkstn <ducquang.kstn@gmail.com>
2021-08-11 17:50:29 +0700

    update echidna test

--- contracts/echidna/BitMathEchidnaTest.sol
+++ contracts/echidna/BitMathEchidnaTest.sol
@@ -2,17 +2,18 @@
 pragma solidity >=0.8.0;
 
 import '../libraries/BitMath.sol';
+import './EchidnaAssert.sol';
 
-contract BitMathEchidnaTest {
-  function mostSignificantBitInvariant(uint256 input) external pure {
+contract BitMathEchidnaTest is EchidnaAssert {
+  function mostSignificantBitInvariant(uint256 input) external {
     uint8 msb = BitMath.mostSignificantBit(input);
-    assert(input >= (uint256(2)**msb));
-    assert(msb == 255 || input < uint256(2)**(msb + 1));
+    isTrue(input >= (uint256(2)**msb));
+    isTrue(msb == 255 || input < uint256(2)**(msb + 1));
   }
 
-  function leastSignificantBitInvariant(uint256 input) external pure {
+  function leastSignificantBitInvariant(uint256 input) external {
     uint8 lsb = BitMath.leastSignificantBit(input);
-    assert(input & (uint256(2)**lsb) != 0);
-    assert(input & (uint256(2)**lsb - 1) == 0);
+    isTrue(input & (uint256(2)**lsb) != 0);
+    isTrue(input & (uint256(2)**lsb - 1) == 0);
   }
 }
--- contracts/echidna/FullMathEchidnaTest.sol
+++ contracts/echidna/FullMathEchidnaTest.sol
@@ -2,22 +2,23 @@
 pragma solidity >=0.8.0;
 
 import '../libraries/FullMath.sol';
+import './EchidnaAssert.sol';
 
-contract FullMathEchidnaTest {
+contract FullMathEchidnaTest is EchidnaAssert {
   function checkMulDivRounding(
     uint256 x,
     uint256 y,
     uint256 d
-  ) external pure {
+  ) external {
     require(d > 0);
 
     uint256 ceiled = FullMath.mulDivCeiling(x, y, d);
     uint256 floored = FullMath.mulDivFloor(x, y, d);
 
     if (mulmod(x, y, d) > 0) {
-      assert(ceiled - floored == 1);
+      isTrue(ceiled - floored == 1);
     } else {
-      assert(ceiled == floored);
+      isTrue(ceiled == floored);
     }
   }
 
@@ -25,43 +26,43 @@ contract FullMathEchidnaTest {
     uint256 x,
     uint256 y,
     uint256 d
-  ) external pure {
+  ) external {
     require(d > 0);
     uint256 z = FullMath.mulDivFloor(x, y, d);
     if (x == 0 || y == 0) {
-      assert(z == 0);
+      isTrue(z == 0);
       return;
     }
 
     // recompute x and y via mulDivFloor of the result of floor(x*y/d), should always be less than original inputs by < d
     uint256 x2 = FullMath.mulDivFloor(z, d, y);
     uint256 y2 = FullMath.mulDivFloor(z, d, x);
-    assert(x2 <= x);
-    assert(y2 <= y);
+    isTrue(x2 <= x);
+    isTrue(y2 <= y);
 
-    assert(x - x2 < d);
-    assert(y - y2 < d);
+    isTrue(x - x2 < d);
+    isTrue(y - y2 < d);
   }
 
   function checkmulDivCeiling(
     uint256 x,
     uint256 y,
     uint256 d
-  ) external pure {
+  ) external {
     require(d > 0);
     uint256 z = FullMath.mulDivCeiling(x, y, d);
     if (x == 0 || y == 0) {
-      assert(z == 0);
+      isTrue(z == 0);
       return;
     }
 
     // recompute x and y via mulDivFloor of the result of floor(x*y/d), should always be less than original inputs by < d
     uint256 x2 = FullMath.mulDivFloor(z, d, y);
     uint256 y2 = FullMath.mulDivFloor(z, d, x);
-    assert(x2 >= x);
-    assert(y2 >= y);
+    isTrue(x2 >= x);
+    isTrue(y2 >= y);
 
-    assert(x2 - x < d);
-    assert(y2 - y < d);
+    isTrue(x2 - x < d);
+    isTrue(y2 - y < d);
   }
 }
