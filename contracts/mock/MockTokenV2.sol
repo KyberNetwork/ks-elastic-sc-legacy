a120d3e1fbc7faec9154fa2d1a2c3e21a8cb33e4
Tran Vu <vutt94@gmail.com>
2023-04-24 16:57:13 +0700

    Fix: update UT to cover all decimals (18x18)

--- contracts/mock/MockTokenV2.sol
+++ contracts/mock/MockTokenV2.sol
@@ -10,11 +10,19 @@ import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
 contract MockTokenV2 is ERC20, Ownable {
   using SafeERC20 for IERC20;
 
-  constructor(string memory _name, string memory _symbol, uint256 _supply) ERC20(_name, _symbol) {
+  uint8 public decimal;
+
+  constructor(
+    string memory _name,
+    string memory _symbol,
+    uint256 _supply,
+    uint8 _decimal
+  ) ERC20(_name, _symbol) {
+    decimal = _decimal;
     _mint(msg.sender, _supply);
   }
 
   function decimals() public view virtual override returns (uint8) {
-    return 2;
+    return decimal;
   }
 }
